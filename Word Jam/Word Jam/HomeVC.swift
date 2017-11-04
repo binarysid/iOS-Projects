d36dc34350d1e8c3e83c3d04e5ecd892f12c7133
Linkon <binarysid@users.noreply.github.com>
2017-11-04 09:42:08 +0600

    Update HomeVC.swift

--- Word Jam/Word Jam/HomeVC.swift
+++ Word Jam/Word Jam/HomeVC.swift
@@ -15,6 +15,7 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     let store = DataStore() // local data storage object
     let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
     let placeHolder = "type word of the day"
+    
     // become first responder to get shake motion
     override var canBecomeFirstResponder: Bool {
         get {
@@ -23,6 +24,7 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     }
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         self.view.backgroundColor = Resources.appBackgroundColor
         self.configureAWSService()
@@ -38,21 +40,20 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
         self.wordTextField.text = self.placeHolder
         self.wordTextField.textColor = UIColor.lightGray
         self.wordTextField.becomeFirstResponder()
-        
-        //self.wordTextField.selectedTextRange = self.wordTextField.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
-        //self.wordTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: UIControlEvents.editingChanged)
+     
     }
     
     private func setBandLabelAction(label:UILabel){
+        
         label.isUserInteractionEnabled = true
         let bandTapGesture = UITapGestureRecognizer(target: self, action: #selector(HomeVC.bandTapped(_:)))
         bandTapGesture.delegate = self as UIGestureRecognizerDelegate
         label.addGestureRecognizer(bandTapGesture)
     }
     
-    func bandTapped(_ sender: UITapGestureRecognizer) {
+    func bandTapped(_ sender: UITapGestureRecognizer) { // when word of the day box is pressed
+        
         let alertController = UIAlertController(title: self.store.getWordDefinition().word, message: "\(self.store.getWordDefinition().meaning)", preferredStyle: UIAlertControllerStyle.alert)
-
         let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
             print("OK")
         }
@@ -62,20 +63,23 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
         super.viewWillTransition(to: size, with: coordinator)
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
-        self.setNavigationBarItem()
+        self.setNavigationBarItem() // set the header navigation bar
     }
 
     // Enable detection of shake motion
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if motion == .motionShake {
+        
+        if motion == .motionShake { // if shake gesture detected
             //print("shake shake")
             if let label = self.showToast(Resources.bandToastColor, textColor: UIColor.white, message: self.store.getWordDefinition().word, position: TOASTPOSITIONTOP){
-                self.setBandLabelAction(label: label)
+                self.setBandLabelAction(label: label) // set touch action for the band message
                 self.view.addSubview(label)
                 let when = DispatchTime.now() + 5 // 5 seconds delay
                 DispatchQueue.main.asyncAfter(deadline: when) {
@@ -87,8 +91,6 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     
     func textViewDidChange(_ textView: UITextView) {
         
-        //let wordDef = self.store.getWordDefinition() // get word from local storage
-
         if textView.text.lowercased().range(of:self.store.getWordDefinition().word ) != nil{ // when user input match with word of the day publish it to AWS SNS topic
             self.publishToSNSTopic(userInput:textView.text!)
         }
@@ -96,6 +98,7 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     }
 
     private func configureAWSService(){
+        
         let credentialsProvider = AWSCognitoCredentialsProvider(regionType: self.regionType, identityPoolId: self.identityPoolID)
         let configuration = AWSServiceConfiguration(region: self.regionType, credentialsProvider: credentialsProvider)
         AWSServiceManager.default().defaultServiceConfiguration = configuration
@@ -103,25 +106,24 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
     }
     
     private func publishToSNSTopic(userInput:String) {
+        
         let sns = AWSSNS.default()
         let request = AWSSNSPublishInput()
         request?.messageStructure = "json"
         
         do {
-            //self.view.endEditing(true)
-            //self.showLoading("Sending to AWS")
-            
+            // converts user string to json data format
             let jsonData = try JSONSerialization.data(withJSONObject: self.getJsonData(userInput:userInput), options: [])
             request?.message = NSString(data: jsonData, encoding: String.Encoding.utf8.rawValue) as String?
             request?.targetArn = self.snsTopic
             
             sns.publish(request!).continueWith { (task) -> AnyObject! in
+                                                
                 print(request?.message ?? "")
-                //self.hideLoading()
-                if task.error != nil {
+                if task.error != nil { // if request fails
                     print("error \(String(describing: task.error)), result:; \(String(describing: task.result))")
                 }
-                else {
+                else { // if request succeeded
                    
                     if task.isCompleted{
                         print("complete");
@@ -143,54 +145,40 @@ class HomeVC: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate 
         
     }
     
-    private func getJsonData(userInput:String)->Dictionary<String, String>{
+    private func getJsonData(userInput:String)->Dictionary<String, String>{ // get data as dictionary format
         
-        let wordDef = self.store.getWordDefinition() // get word information
-        
-        
-//        if let dictionaryJson = convertToDictionary(text: (wordDef.apiResult)){
-//            //print(wordDef.apiResult)
-//        // convert [String:Any] to [String:String]
-//            dict.update(other: convertStringAnyObjectToString(dict: dictionaryJson)!)
-//        }
+        let wordDef = self.store.getWordDefinition() // get word information from local storage
+        // json input to be sent
         let dict = ["default": "The default message","name": self.store.getUserName(),"userMessage":userInput, "source":wordDef.source, "apiResult": wordDef.apiResult]
 
         return dict
     }
+    
     func textViewDidBeginEditing(_ textView: UITextView) {
+        
         if textView.textColor == UIColor.lightGray {
             textView.text = nil
             textView.textColor = UIColor.black
         }
     }
+    
     func textViewDidEndEditing(_ textView: UITextView) {
+        
         if textView.text.isEmpty {
             textView.text = self.placeHolder
             textView.textColor = UIColor.lightGray
         }
     }
-//    func textViewDidBeginEditing(_ textView: UITextView) {
-//        <#code#>
-//    }
-    
-//    func textFieldDidBeginEditing(_ textField: UITextView) {
-//        
-//    }
-//    func textFieldDidEndEditing(_ textField: UITextView) {
-//        animateViewMoving(up: true, moveValue: -(textField.frame.origin.y/2-50))
-//    }
+
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if(text == "\n") {
+        
+        if(text == "\n") { // if return pressed from keyboard
             textView.resignFirstResponder()
             return false
         }
         return true
     }
 
-//    func textFieldShouldReturn(_ textField: UITextView) -> Bool {
-//        textField.resignFirstResponder()
-//        return false
-//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
